387658987500bda5a32f9302ac18011c3292a688
Alex Addy <alex.addy@gmail.com>
2019-12-02 20:01:10 -0700

    Add solution for day 2 part 2

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -1,5 +1,6 @@
 pub fn work(lines: &Vec<String>) {
     println!("Part 1: {}", do_work(&lines));
+    println!("Part 2: {}", do_work_2(&lines));
 }
 
 fn do_work(lines: &Vec<String>) -> usize {
@@ -8,6 +9,22 @@ fn do_work(lines: &Vec<String>) -> usize {
     program[1] = 12;
     program[2] = 2;
     interpret(&mut program)
+}
+
+fn do_work_2(lines: &Vec<String>) -> usize {
+    let program: Vec<usize> = lines[0].split(',').map(|piece| piece.parse().unwrap()).collect();
+    for i in 0..99 {
+        for k in 0..99 {
+            let mut tmp_mem = program.clone();
+            tmp_mem[1] = i; // noun
+            tmp_mem[2] = k; // verb
+            let res = interpret(&mut tmp_mem);
+            if res == 19690720 {
+                return i * 100 + k;
+            }
+        }
+    }
+    panic!("Couldn't find inputs!");
 }
 
 fn interpret(mem: &mut [usize]) -> usize {
